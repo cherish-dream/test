af515533b3d2af76cee33aaf218f5718d1bf69e6
张小厨 <zhangjiesharp@163.com>
2018-06-20 20:54:16 +0800

    图片验证码之前端逻辑实现

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -1,6 +1,6 @@
-var vm = new Vue({
-	el: '#app',
-	data: {
+var vm = new Vue({ // 创建vue实例
+	el: '#app',    // 绑定vue
+	data: {	// 定义要使用的变量
 		error_name: false,
 		error_password: false,
 		error_check_password: false,
@@ -15,9 +15,38 @@ var vm = new Vue({
 		mobile: '', 
 		image_code: '',
 		sms_code: '',
-		allow: false
+		allow: false,
+
+		image_code_id: '', // 图片唯一标识
+		image_code_url: '' // img标签获取图片验证码的url
 	},
-	methods: {
+	mounted: function() { // 文档加载完之后调用方法的
+		// 当文档加载完就自动的获取图片验证码
+		this.generate_image_code();
+	},
+	methods: { // 准备要被调用的方法的
+		// 生成UUID的方法
+		generate_uuid: function(){
+			var d = new Date().getTime();
+			if(window.performance && typeof window.performance.now === "function"){
+				d += performance.now(); //use high-precision timer if available
+			}
+			var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+				var r = (d + Math.random()*16)%16 | 0;
+				d = Math.floor(d/16);
+				return (c =='x' ? r : (r&0x3|0x8)).toString(16);
+			});
+        	return uuid; // image_code_id
+    	},
+
+		// 索取图片验证码方法
+		generate_image_code: function () {
+			// 生成uuid==image_code_id
+			this.image_code_id = this.generate_uuid();
+			// 拼接获取图片验证码的url，赋值给img标签的src属性
+			this.image_code_url = 'http://127.0.0.1:8000' + '/image_codes/' + this.image_code_id + '/';
+        },
+
 		check_username: function (){
 			var len = this.username.length;
 			if(len<5||len>20) {
