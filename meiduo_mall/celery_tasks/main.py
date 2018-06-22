bfec32ebd3643d8d3efeab43610251e4d7fa8dd6
张小厨 <zhangjiesharp@163.com>
2018-06-22 12:35:24 +0800

    Celery之定义异步任务

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -10,3 +10,8 @@ celery_app.config_from_object('celery_tasks.config')
 # 自动将异步任务注册到celery_app
 # 提示：不需要指向tasks.py;因为celery默认回去寻找tasks.py同名的文件
 celery_app.autodiscover_tasks(['celery_tasks.sms'])
+
+
+# 开启celery的worker
+# celery -A celery实例应用包路径 worker -l info
+# celery -A celery_tasks.main worker -l info
--- meiduo_mall/celery_tasks/sms/tasks.py
+++ meiduo_mall/celery_tasks/sms/tasks.py
@@ -1,5 +1,11 @@
+from celery_tasks.sms.yuntongxun.sms import CCP
+from . import constants
+from celery_tasks.main import celery_app
 
 
-def send_sms_code():
+# 使用装饰器将send_sms_code装饰成异步任务，并起别名
+@celery_app.task('send_sms_code')
+def send_sms_code(mobile, sms_code):
     """发送短信异步任务"""
-    pass
+
+    CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SMS_SEND_TEMPLATE_ID)
