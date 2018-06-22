761e91bd1ce62186eebf3e4f439d74707469dade
张小厨 <zhangjiesharp@163.com>
2018-06-22 12:26:24 +0800

    Celery之定义

---
+++ meiduo_mall/celery_tasks/main.py
@@ -0,0 +1,12 @@
+# celery启动入口
+from celery import Celery
+
+# 创建celery客户端,并起别名（没有实际意义）
+celery_app = Celery('meiduo')
+
+# 加载配置:让客户端和worker知道broker的存在
+celery_app.config_from_object('celery_tasks.config')
+
+# 自动将异步任务注册到celery_app
+# 提示：不需要指向tasks.py;因为celery默认回去寻找tasks.py同名的文件
+celery_app.autodiscover_tasks(['celery_tasks.sms'])
---
+++ meiduo_mall/celery_tasks/sms/tasks.py
@@ -0,0 +1,5 @@
+
+
+def send_sms_code():
+    """发送短信异步任务"""
+    pass
