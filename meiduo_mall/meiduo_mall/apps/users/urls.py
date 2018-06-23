406f16d3f01af919e687bd4e7cc016033a61f1ac
张小厨 <zhangjiesharp@163.com>
2018-06-23 11:27:24 +0800

    登录之JWT辅助登录

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
     # 注册
     url(r'^users/$', views.UserView.as_view()),
+    # JWT辅助登录
+    url(r'^authorizations/$', obtain_jwt_token),
 ]
