5ee1d5781d74c5627d9bc5269aa258627d2f7a22
张小厨 <zhangjiesharp@163.com>
2018-06-25 10:54:56 +0800

    用户扫码后进入回调页面接口定义

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    # 获取QQ登录扫码界面
     url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
+    # 获取QQ用户的openid(用户信息)
+    url(r'^qq/user/$', views.QQAuthUserView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -6,14 +6,30 @@ from .utils import OAuthQQ
 # Create your views here.
 
 
+# url(r'^qq/user/$', views.QQAuthUserView.as_view()), 
+class QQAuthUserView(APIView):
+    """用户扫码登录的回调处理"""
+    def get(self, request):
+        # 提取code请求参数
+        # 使用code向QQ服务器请求access_token
+        # 使用access_token向QQ服务器请求openid
+        # 使用openid查询该QQ用户是否在美多商城中绑定过用户
+        # 如果openid已绑定美多商城用户，直接生成JWT token，并返回
+        # 如果openid没绑定美多商城用户，创建用户并绑定到openid
+        pass
+
+
 #  url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
 class QQAuthURLView(APIView):
     """提供QQ登录界面的URL"""
 
     def get(self, request):
 
+        # 获取next
+        next = request.query_params.get('next')
+
         # 创建oauth工具对象
-        oauth = OAuthQQ()
+        oauth = OAuthQQ(state=next)
 
         # 调用获取QQ登录界面的URL的工具方法
         login_url =  oauth.get_qq_login_url()
