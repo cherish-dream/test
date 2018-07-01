6588375fdb183bdac899a01bf528198816b7579a
张小厨 <zhangjiesharp@163.com>
2018-07-01 11:41:00 +0800

    商品列表页之商品分类静态化

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -54,7 +54,19 @@ class SKUImageAdmin(admin.ModelAdmin):
         generate_static_sku_detail_html.delay(sku_id)
 
 
-admin.site.register(models.GoodsCategory)
+class GoodsCategoryAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_list_search_html
+        generate_static_list_search_html.delay()
+
+    def delete_model(self, request, obj):
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_list_search_html
+        generate_static_list_search_html.delay()
+
+
+admin.site.register(models.GoodsCategory, GoodsCategoryAdmin)
 admin.site.register(models.GoodsChannel)
 admin.site.register(models.Goods)
 admin.site.register(models.Brand)
