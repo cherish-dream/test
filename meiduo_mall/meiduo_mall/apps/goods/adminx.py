2005a964c7522af014961c887633480ee9c53744
张小厨 <zhangjiesharp@163.com>
2018-07-07 12:15:04 +0800

    Xadmin之展示模型数据

---
+++ meiduo_mall/meiduo_mall/apps/goods/adminx.py
@@ -0,0 +1,14 @@
+import xadmin
+
+from . import models
+
+
+xadmin.site.register(models.GoodsCategory)
+xadmin.site.register(models.GoodsChannel)
+xadmin.site.register(models.Goods)
+xadmin.site.register(models.Brand)
+xadmin.site.register(models.GoodsSpecification)
+xadmin.site.register(models.SpecificationOption)
+xadmin.site.register(models.SKU)
+xadmin.site.register(models.SKUSpecification)
+xadmin.site.register(models.SKUImage)
